b51892a6bea9a182674c17d16e88f4e5dfbc0e22
Girish144 <girishkumbar100@gmail.com>
2026-03-25 21:56:00 +0530

    completed count number

---
+++ countnumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class addnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while (num > 0) {
+            int digit = num % 10;   // get last digit
+
+            if (digit == 2) {
+                count++;
+            }
+
+            num = num / 10;   // remove last digit
+        }
+
+        System.out.println("Number of times 2 appears: " + count);
+    }
+}
